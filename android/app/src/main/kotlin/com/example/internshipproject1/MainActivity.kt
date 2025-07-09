2ecff22c168341922ab5862ef7044d47bc2699d7
roquia-munir06 <169173891+roquia-munir06@users.noreply.github.com>
2025-07-09 09:29:17 -0700

    abbasi

---
+++ android/app/src/main/kotlin/com/example/internshipproject1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.internshipproject1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/registration.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:internshipproject1/screens/registration2.dart';
+
+class Registration extends StatelessWidget {
+  const Registration({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white
+            ),
+          ),
+          // 🔹 Background Image
+          Image.asset(
+            'assets/images/regback.jpg', // Make sure this image exists in assets
+            fit: BoxFit.cover,
+          ),
+
+          // 🔹 Overlapping White Container (Starting inside image)
+          Positioned(
+            top: 180,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: screenHeight*0.55,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30,right: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Title
+                      SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        "Create Account",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 7),
+                      const Text(
+                        "Login to Access Your Account.",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(height: 30),
+                      TextField(
+                        controller: TextEditingController(text: "khurram abbasi"),
+                        decoration: InputDecoration(
+                          labelText: "Name",
+                          labelStyle: const TextStyle(color: Colors.green),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.green, width: 2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+                      // Email field
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: const TextStyle(color: Colors.green),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.green, width: 2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+                      // Password field
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelStyle: const TextStyle(color: Colors.green),
+                          labelText: "Password",
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.green, width: 2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+                      // Confirm Password field
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Confirm Password",
+                          labelStyle: const TextStyle(color: Colors.green),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.green, width: 2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 58),
+                      // Next button
+                      SizedBox(
+                        width: 315,
+                        height: 60,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=>registration2()));
+                            // Handle next
+                          },
+                          child: const Text("Next"),
+                        ),
+                      ),
+
+                      const SizedBox(height: 15),
+                      // Login row
+                      const Text("Already have an account? ",style: TextStyle(color: Colors.grey),),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigate to login
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 0.2),
+                          child: TextButton(onPressed: (){},
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 27.65,
+                                fontFamily: "Raleway",
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
